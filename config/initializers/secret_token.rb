5b37fc13c75ca21149a093a3e8894680d35a3697
unknown <Kalmh@Kalmh-PC.(none)>
2012-05-18 16:04:07 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LePtiCoin::Application.config.secret_token = '16441898bcf9362ee423a132762781205d0c8ee3461cc2d1933e49e0b77b29b0a3e9b9af60caa538c07cca3ca9b460b049b749ed14423ad42fdfa755027339c1'
